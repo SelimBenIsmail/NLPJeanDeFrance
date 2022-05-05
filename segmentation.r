047ae1a1310849e29e4f1baa6b5aabfdf1e4f7d3
SelimBenIsmail <selim.doms@gmail.com>
2022-05-05 16:39:35 +0200

    add

--- segmentation.r
+++ segmentation.r
@@ -21,9 +21,9 @@ df_main <- fullExtract(text)
 names(df_main)[1:7] <- c("numEscroete", "escroete", "numConnetablie", "connetablie", "rdv", "numRente","rente")
 
 ## Récupération des tables ##
-write.csv(x=df_main, file="Export/df_main.csv", row.names = TRUE)
-write.csv(x=df_connetablies, file="Export/df_connetablies.csv", row.names = TRUE)
-write.csv(x=df_rentes, file="Export/df_rentes.csv", row.names = TRUE)
+write.csv(x=df_main, file="export/df_main.csv", row.names = TRUE)
+write.csv(x=df_connetablies, file="export/df_connetablies.csv", row.names = TRUE)
+write.csv(x=df_rentes, file="export/df_rentes.csv", row.names = TRUE)
 
 
 
