5129b4d3f04a03b578f47c1a4becbe406f598b71
SelimBenIsmail <selim.doms@gmail.com>
2022-05-11 11:22:53 +0200

    architecture modified

--- main.R
+++ main.R
@@ -6,4 +6,4 @@ source("./segmentation/functions_Seg.R")
 
 #### Execution ####
 dataImported <- scan(file = "./sources/extrait_p37_p57.txt", what = "string")
-source("./segmentation/segmentation.r")
+source("./Extraction_de_l_information/segmentation.R")
